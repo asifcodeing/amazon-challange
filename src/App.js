569fd395f358d9d81493836b6e7a13091c6b869b
asifcodeing <asifshaikh24061996@gmail.com>
2020-12-15 15:28:26 +0530

    firebase config

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    //BEM
+    <div className="app">
       <h1>Hey Asif Shaikh let build Amazon Clone</h1>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZfu-cb85szNvu_sWwY3Ua5KwVq4ocfpg",
+    authDomain: "e-challange-397f6.firebaseapp.com",
+    projectId: "e-challange-397f6",
+    storageBucket: "e-challange-397f6.appspot.com",
+    messagingSenderId: "435726988061",
+    appId: "1:435726988061:web:816d33e93912527feaa7f4",
+    measurementId: "G-316940SKMK"
+};
